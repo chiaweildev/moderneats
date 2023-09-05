e8a0a0319023319c3fd55d05e79772c2362edb31
martin <chiaweildev@gmail.com>
2023-09-06 07:08:26 +0800

    feat: remove main tag in Navigation component.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,24 +5,22 @@ import Location from "@/components/Location";
 
 const Navigation = () => {
   return (
-    <main>
-      <div className="flex min-h-[96px] items-center px-10">
-        <div className="flex items-center">
-          <HamburgerMenuIcon className="mr-8 h-5 w-5 cursor-pointer"  />
-          <a href="/" className="text-[28px]">
-            <span className=" text-[34px] font-light">m</span>
-            odern<b className="mx-2">Eats</b>
-          </a>
-        </div>
-        <div className="ml-8 mr-12">
-          <Location />
-        </div>
-        <div className="flex flex-1 items-center justify-between">
-          <SearchBar />
-          <Cart />
-        </div>
-      </div>
-    </main>
+    <div className="flex min-h-[96px] items-center px-10">
+    <div className="flex items-center">
+        <HamburgerMenuIcon className="mr-8 h-5 w-5 cursor-pointer"  />
+        <a href="/" className="text-[28px]">
+        <span className=" text-[34px] font-light">m</span>
+        odern<b className="mx-2">Eats</b>
+        </a>
+    </div>
+    <div className="ml-8 mr-12">
+        <Location />
+    </div>
+    <div className="flex flex-1 items-center justify-between">
+        <SearchBar />
+        <Cart />
+    </div>
+    </div>
   );
 };
 
