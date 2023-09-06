ed53fe5a5dce48040c958b0043d04bf02a26cd6a
martin <chiaweildev@gmail.com>
2023-09-06 23:36:17 +0800

    feat: modify Cart  height and SearchBar width.

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -4,10 +4,10 @@ const Cart = () => {
   return (
     <div
       className="flex cursor-pointer select-none items-center gap-3 rounded-3xl bg-black
-           px-4 py-2 text-sm text-slate-50 hover:opacity-80"
+           px-4 py-2 h-[48px] text-sm text-slate-50 hover:opacity-80 text-[16px]"
     >
       <BackpackIcon className="h-4 w-4" />
-      購物車
+      0 台購物車
     </div>
   );
 };
