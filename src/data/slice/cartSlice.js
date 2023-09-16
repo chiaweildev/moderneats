c64131c71a118fb35b7806f0b4731ae500880231
martin <chiaweildev@gmail.com>
2023-09-17 00:44:50 +0800

    feat: create slice for redux.

---
+++ src/data/slice/cartSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        add: (state, {payload})=>{
+            if (state[payload.id]) {
+                state[payload.id] +=1
+            } else {
+                state[payload] = 1;
+            }
+        },
+        remove: (state, {payload})=>{
+            if (state[payload.id]) {
+                state[payload.id] -=1
+            } 
+        }
+    }
+})
+
+export const {add, remove } = cartSlice.actions
+export default cartSlice.reducer
